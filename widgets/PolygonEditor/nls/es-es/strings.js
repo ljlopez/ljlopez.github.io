447c941386e5be023fa294e169176c30e1948ec9
ljlopez <javi.lop3z@gmail.com>
2016-11-14 01:08:50 +0100

    Add files via upload

---
+++ widgets/PolygonEditor/nls/es-es/strings.js
@@ -0,0 +1,7 @@
+﻿define({
+  test1 : "Éste es mi widget.",
+  undoRedoToolbar: "Deshacer / Rehacer",
+  selectionToolbar: "Seleccionar",
+  editToolbar: "Editar",
+  copyPasteToolbar: "Copiar / Pegar"
+});
---
+++ widgets/PolygonEditor/nls/strings.js
@@ -0,0 +1,10 @@
+﻿define({
+  root:{
+    test1: "This is my widget.",
+    undoRedoToolbar: "Undo / Redo",
+    selectionToolbar: "Select",
+    editToolbar: "Edit",
+    copyPasteToolbar: "Copy / Paste"
+  },
+  "es-es": true    
+});
